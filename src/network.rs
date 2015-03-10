7ef0ef12981d656c4287664846dea04ae930481d
Tom Burdick <thomas.burdick@gmail.com>
2015-03-10 01:27:15 -0400

    less mutable in network creation

--- src/network.rs
+++ src/network.rs
@@ -12,10 +12,8 @@ pub struct Network {
 
 impl Network {
     pub fn new(input_sizes : &Vec<usize>, outputs : usize) -> Network {
-        let mut output_sizes : Vec<usize> = input_sizes.iter().cloned().skip(1).collect();
-        output_sizes.push(outputs);
+        let output_sizes : Vec<usize> = input_sizes.iter().skip(1).chain(vec!(outputs).iter()).cloned().collect();
         let inputs_outputs : Vec<(usize, usize)> = input_sizes.iter().cloned().zip(output_sizes.iter().cloned()).collect();
-        println!("{:?}", inputs_outputs);
         return Network {
             layers: inputs_outputs.iter().map(|&(inputs, outputs)| Layer::new(inputs, outputs)).collect()
         }
